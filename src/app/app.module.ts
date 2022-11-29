aef5eb199b2b7f7d3446c61f0866518fa94475ca
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-11-29 18:32:50 +0530

    add services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ViewCourseComponent } from './view-course/view-course.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { Route, RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule }from '@angular/common/http'
 const myRoute:Routes=[
   {
     path:"",
@@ -30,7 +31,8 @@ const myRoute:Routes=[
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot(myRoute),
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
